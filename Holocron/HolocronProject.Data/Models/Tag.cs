0403ad7a02e5288d3ef7622da4833900f39e8ee1
Krasimir D <detha4545@gmail.com>
2020-10-07 20:36:54 +0300

    Fix an issue with the last commit

--- Holocron/HolocronProject.Data/Models/Tag.cs
+++ Holocron/HolocronProject.Data/Models/Tag.cs
@@ -1,35 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
-
-namespace HolocronProject.Data.Models
-{
-    public class Tag
-    {
-        public int Id { get; set; }using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using HolocronProject.Data.Common;
 
 namespace HolocronProject.Data.Models
+{
+    public class Tag
     {
-        public class Tag
-        {
-            public int Id { get; set; }
+        public int Id { get; set; }
 
-            [MaxLength(GlobalConstants.TagConstants.NameMaxLength)]
-            public string Name { get; set; }
+        [MaxLength(GlobalConstants.TagConstants.NameMaxLength)]
+        public string Name { get; set; }
 
-            public ICollection<PostTag> Posts { get; set; } = new List<PostTag>();
-        }
-    }
-
-    // TODO: Add a max lenght (add a common)
-    public string Name { get; set; }
-
-        public ICollection<PostTag> Posts { get; set; } = new List<PostTag>(); 
+        public ICollection<PostTag> Posts { get; set; } = new List<PostTag>();
     }
 }
