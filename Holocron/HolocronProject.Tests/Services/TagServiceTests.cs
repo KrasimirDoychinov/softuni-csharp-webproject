cff386d08a5403a10294fad85525a4d8432ea20f
Krasimir D <detha4545@gmail.com>
2020-11-03 18:07:02 +0200

    Add TagServiceTests

---
+++ Holocron/HolocronProject.Tests/Services/TagServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using HolocronProject.Data;
+using HolocronProject.Services;
+using HolocronProject.Services.Implementations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolocronProject.Tests.Services
+{
+    [TestFixture]
+    public class TagServiceTests
+    {
+        private HolocronDbContext context;
+        private ITagService tagService;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            this.context = new HolocronDbContext();
+            this.tagService = new TagService(context);
+
+            await this.context.Database.EnsureDeletedAsync();
+            await this.context.Database.EnsureCreatedAsync();
+        }
+
+        [Test]
+        public async Task CreateTagCreatesTagAndIncreaesCount()
+        {
+            await this.tagService.CreateTag("Test");
+
+            var actualResult = this.context.Tags
+                .Count();
+            var expectedResult = 1;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
