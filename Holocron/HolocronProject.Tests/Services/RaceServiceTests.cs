adee09f04dec4bb01beafcf9a2ad99fc02333ec7
Krasimir D <detha4545@gmail.com>
2020-11-03 17:59:30 +0200

    Add RaceServiceTests

---
+++ Holocron/HolocronProject.Tests/Services/RaceServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using HolocronProject.Data;
+using HolocronProject.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolocronProject.Tests.Services
+{
+    [TestFixture]
+    public class RaceServiceTests
+    {
+        private HolocronDbContext context;
+        private IRaceService raceService;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            this.context = new HolocronDbContext();
+            this.raceService = new RaceService(context);
+
+            await this.context.Database.EnsureDeletedAsync();
+            await this.context.Database.EnsureCreatedAsync();
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await context.DisposeAsync();
+        }
+
+        [Test]
+        public async Task CreateRaceCreatesRaceAndIncreasesCount()
+        {
+            await this.raceService.CreateRace("Test");
+
+            var actualResult = this.context.Races
+                .Count();
+            var expectedResult = 13;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
