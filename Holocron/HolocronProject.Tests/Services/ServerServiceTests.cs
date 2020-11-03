07cc8fae311107730cca5fcb7566b4daccbd9e1d
Krasimir D <detha4545@gmail.com>
2020-11-03 18:03:33 +0200

    Add ServerServiceTests

---
+++ Holocron/HolocronProject.Tests/Services/ServerServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using HolocronProject.Data;
+using HolocronProject.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolocronProject.Tests.Services
+{
+    [TestFixture]
+    public class ServerServiceTests
+    {
+        private HolocronDbContext context;
+        private IServerService serverService;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            this.context = new HolocronDbContext();
+            this.serverService = new ServerServices(context);
+
+            await this.context.Database.EnsureDeletedAsync();
+            await this.context.Database.EnsureCreatedAsync();
+        }
+
+        [Test]
+        public async Task CreateRaceCreatesRaceAndIncreasesCount()
+        {
+            await this.serverService.CreateServer("Test");
+
+            var actualResult = this.context.Servers
+                .Count();
+            var expectedResult = 6;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
