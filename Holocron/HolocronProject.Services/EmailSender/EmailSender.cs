441fc7d5f45e25e03ce7d41d8f8a86c2c83cc046
Krasimir D <detha4545@gmail.com>
2020-12-22 20:29:41 +0200

    Add email verification when registering

---
+++ Holocron/HolocronProject.Services/EmailSender/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace HolocronProject.Services.EmailSender
+{
+    public class EmailSender : IEmailSender
+    {
+        public void SendConfirmationEmail(Data.Models.Account account, string confirmationLink)
+        {
+            confirmationLink = confirmationLink.Replace("/Identity", "");
+            MailMessage mail = new MailMessage();
+            mail.To.Add(account.Email);
+
+            mail.From = new MailAddress("holocronAutomaticSender@gmail.com");
+            mail.Subject = "Holocron verification email";
+
+
+            mail.Body = $"<p>Your verification link: <a href=\"{confirmationLink}\">Click me.</a></p>";
+
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Credentials = new System.Net.NetworkCredential
+                 ("holocronAutomaticSender@gmail.com", "holocronAutomaticSender1!");
+
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+        
+    }
+}
---
+++ Holocron/HolocronProject.Services/EmailSender/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HolocronProject.Services.EmailSender
+{
+    public interface IEmailSender
+    {
+        void SendConfirmationEmail(Data.Models.Account account, string confirmationLink);
+    }
+}
